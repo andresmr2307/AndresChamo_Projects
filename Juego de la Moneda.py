4d9826f4b5fd85b314e18d01b2ab9ce53452f381
andresmr2307 <andresmr2307@users.noreply.github.com>
2019-08-29 11:31:03 -0500

    my basic projectss
    xd

---
+++ Juego de la Moneda.py
@@ -0,0 +1,19 @@
+import random
+import time
+
+
+
+lanzamientos = 0
+cara = "Cara"
+sello = "Sello"
+
+cara_y_sello = [(cara), (sello)]
+while input("Quieres lanzar una moneda? [y|n]: \n") == 'y':
+    print(random.choice(cara_y_sello))
+    time.sleep(.5)
+    lanzamientos += 1
+
+
+else:
+    print("Haz lanzado la moneda",lanzamientos,"times")
+    print("Blaos")
